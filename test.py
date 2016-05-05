5f1f0a0192f39032f7ab8e0c8f092d6ecc9e9234
Bharat Chhabra <bchhabra2490@gmail.com>
2016-05-06 02:29:59 +0530

    Added a new attribute-size

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import tkMessageBox as mbox
 import os
 import csv
 
-
 class Example(Frame):
   
     def __init__(self, parent,theta,m,attr):
@@ -15,7 +14,6 @@ class Example(Frame):
         self.parent = parent        
         self.initUI()
         
-        
     def initUI(self):
       
         self.parent.title("Property Rate Prediction System")
@@ -24,70 +22,80 @@ class Example(Frame):
         menubar = Menu(self.parent)
         self.parent.config(menu=menubar)
         wel = Label(self, text="Welcome to property rate predictor", font=("Helvetica", 30))
-        wel.place( x=200,y=150)
+        wel.place( x=200,y=10)
         fileMenu = Menu(menubar)       
         fileMenu.add_command(label="Exit", underline=0, command=self.onExit)
 
 
         menubar.add_cascade(label="File", underline=0, menu=fileMenu)       
         
-        L1 = Label(self, text="Size of property")
-        L1.place( x=20,y=30)
+
+        L1 = Label(self, text="Num of bedrooms")
+        L1.place( x=20,y=50)
         E1 = Entry(self, bd =5)
-        E1.place( x=150,y=30)
+        E1.insert(0,'Only Integer')
+        E1.place( x=150,y=50)
 
         L2 = Label(self, text="Swimming pool")
-        L2.place( x=20,y=60)
+        L2.place( x=20,y=80)
         E2 = Entry(self, bd =5)
-        E2.place( x=150,y=60)
+        E2.insert(0,'0->N0,1->Yes')
+        E2.place( x=150,y=80)
 
         L3 = Label(self, text="type")
-        L3.place( x=20,y=90)
+        L3.place( x=20,y=110)
         E3 = Entry(self, bd =5)
-        E3.place( x=150,y=90)
+        E3.insert(0,'0->Bungalow,1->Apartment')
+        E3.place( x=150,y=110)
 
         L4 = Label(self, text="Interior")
-        L4.place( x=20,y=120)
+        L4.place( x=20,y=140)
         E4 = Entry(self, bd =5)
-        E4.place( x=150,y=120)
+        E4.insert(0,'Rate from 0 to 5')
+        E4.place( x=150,y=140)
 
         L5 = Label(self, text="Lawn")
-        L5.place( x=20,y=150)
+        L5.place( x=20,y=170)
         E5 = Entry(self, bd =5)
-        E5.place( x=150,y=150)
+        E5.insert(0,'0->N0,1->Yes')
+        E5.place( x=150,y=170)
 
         L6 = Label(self, text="View")
-        L6.place( x=20,y=180)
+        L6.place( x=20,y=200)
         E6 = Entry(self, bd =5)
-        E6.place( x=150,y=180)
+        E6.insert(0,'Rate from 0 to 5')
+        E6.place( x=150,y=200)
 
 
         L7 = Label(self, text="Locality")
-        L7.place( x=20,y=210)
+        L7.place( x=20,y=230)
         E7 = Entry(self, bd =5)
-        E7.place( x=150,y=210)
+        E7.insert(0,'Rate from 0 to 5')
+        E7.place( x=150,y=230)
 
         L8 = Label(self, text="Bathrooms")
-        L8.place( x=20,y=240)
+        L8.place( x=20,y=260)
         E8 = Entry(self, bd =5)
-        E8.place( x=150,y=240)
+        E8.insert(0,'Only Integer')
+        E8.place( x=150,y=260)
 
         L9 = Label(self, text="Parking")
-        L9.place( x=20,y=270)
+        L9.place( x=20,y=290)
         E9 = Entry(self, bd =5)
-        E9.place( x=150,y=270)
+        E9.insert(0,'0->N0,1->Yes')
+        E9.place( x=150,y=290)
 
-        cal=Button(self, text="Calculate", command=lambda:self.cal(filename,E1.get(),E2.get(),E3.get(),E4.get(),E5.get(),E6.get(),E7.get(),E8.get(),E9.get()))
-        cal.place( x=100,y=300)
+        cal=Button(self, text="Calculate", command=lambda:self.cal(E1.get(),E2.get(),E3.get(),E4.get(),E5.get(),E6.get(),E7.get(),E8.get(),E9.get()))
+        cal.place( x=100,y=390)
 
-        exit=Button(self, text="Calculate", command=self.onExit)
-        cal.place( x=200,y=300)
+        exit=Button(self, text="Exit", command=self.onExit)
+        exit.place( x=200,y=390)
 
-    def cal(self,filename,size,pool,typ,interior,lawn,view,locality,bathrooms,parking):
+    def cal(self,bedrooms,pool,typ,interior,lawn,view,locality,bathrooms,parking):
             #all the alculation goes here
             ##room,type,interior,lawn,swimming,view,locality,bathrooms,parking,price
             newdata=[]
-            newdata.append(size)
+            newdata.append(bedrooms)
             newdata.append(typ)
             newdata.append(interior)
             newdata.append(lawn)
@@ -168,10 +176,11 @@ def main():
     for i in range(attr+1):
         theta.append(1)
     minCost(data,theta,m,attr)
+    print theta
     newdata=[]
     root = Tk()
     ex = Example(root,theta,m,attr)
-    root.geometry("450x350+300+300")
+    root.geometry("950x650+300+300")
     root.mainloop()  
 if __name__ == '__main__':
     main()  
